face73583a0c845d45db8d2c07df7e8c014b95aa
MarcelaDelgado <celeste1@hotmail.co.nz>
2020-10-14 22:00:05 +1300

    test

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div id="footer">
-      Copyright &copy; black_white | <a href="http://validator.w3.org/check?uri=referer">HTML5</a> | <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a> | <a href="http://www.html5webtemplates.co.uk">Free CSS Templates</a>
+      Copyright &copy; Ongaonga Bed and Breakfast 2020 | <a href="http://validator.w3.org/check?uri=referer">HTML5</a> | <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a> | <a href="http://www.html5webtemplates.co.uk">Free CSS Templates</a>
     </div>
   </div>
 </body>
