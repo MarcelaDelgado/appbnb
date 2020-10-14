4c52cd3dd6d5ac88d4e175f537ec07db0efa73b0
MarcelaDelgado <celeste1@hotmail.co.nz>
2020-10-14 18:57:36 +1300

    test2

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?
-echo hello;
+include "header.php";
+include "menu.php";
+echo '<div id="site_content">';
+include "sidebar.php";
+echo '<div id="content">';
+include "content.php";
+echo '</div></div>';
+include "footer.php";
+
 
 ?>
