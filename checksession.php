73679d8218d61ffdaf9c9ec82fe47d59999379d4
MarcelaDelgado <40555006+MarcelaDelgado@users.noreply.github.com>
2020-10-16 18:54:10 +1300

    Update checksession.php

--- checksession.php
+++ checksession.php
@@ -7,8 +7,8 @@ function checkUser() {
     if ($_SESSION['loggedin'] == 1)
        return TRUE;
     else {
-       $_SESSION['URI'] = 'http:// '.$_SERVER['REQUEST_URI']; //save current url for redirect     
-       header('Location: http://  /login.php', true, 303);       
+       $_SESSION['URI'] = 'http://us-cdbr-east-02.cleardb.com '.$_SERVER['REQUEST_URI']; //save current url for redirect     
+       header('Location: http://us-cdbr-east-02.cleardb.com/appbnb/login.php', true, 303);       
     } 
 }
  
@@ -29,7 +29,7 @@ function register($username,$password) {
    if ($_SESSION['register'] == 0 and !empty($_SESSION['URI'])) 
         $uri = $_SESSION['URI'];  
    else { 
-     $_SESSION['URI'] =  'http://  /registercustomer.php';  
+     $_SESSION['URI'] =  'http://us-cdbr-east-02.cleardb.com/appbnb/registercustomer.php';  
      $uri = $_SESSION['URI'];
    }  
 
@@ -49,7 +49,7 @@ function login($id,$username) {
    if ($_SESSION['loggedin'] == 0 and !empty($_SESSION['URI']))          
         $uri = $_SESSION['URI'];          
    else { 
-     $_SESSION['URI'] =  'http://   /listrooms.php';         
+     $_SESSION['URI'] =  'http://us-cdbr-east-02.cleardb.com/appbnb/listrooms.php';         
      $uri = $_SESSION['URI'];
    }     
    $_SESSION['loggedin'] = 1;
@@ -66,7 +66,7 @@ function logout(){
   $_SESSION['userid'] = -1;        
   $_SESSION['username'] = '';
   $_SESSION['URI'] = '';
-  header('Location: http://   /login.php', true, 303);    
+  header('Location: http://us-cdbr-east-02.cleardb.com/appbnb/login.php', true, 303);    
 }
 
 
