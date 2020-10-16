c3880073210039f083e0e3e88a9c4edc6f8fe1d3
MarcelaDelgado <40555006+MarcelaDelgado@users.noreply.github.com>
2020-10-16 18:55:41 +1300

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 //MySQL credentails
-//define("DBHOST","");
-define("DBUSER","root");
-define("DBPASSWORD","root");
-define("DBDATABASE","bnb");
+ define("DBHOST","us-cdbr-east-02.cleardb.com");
+ define("DBUSER","b8bdee72cc05ba");
+define("DBPASSWORD","7dc810c4");
+define("DBDATABASE","heroku_a3952f44fb37877");
 
 ?>
